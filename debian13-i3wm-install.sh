f870e412ecb74cf1f5b8630366e4ae8ab1038c75
laimusss <48787750+laimusss@users.noreply.github.com>
2026-04-13 19:35:03 +0300

    Update debian13-i3wm-install.sh

--- debian13-i3wm-install.sh
+++ debian13-i3wm-install.sh
@@ -42,6 +42,36 @@ else
     sudo -u "$ORIGINAL_USER" xdg-user-dirs-update --force
 fi
 
+# Themes
+# Путь к директории
+themes_dir="/home/$username/.themes"
+
+# Проверяем, существует ли директория, и если нет — создаём её
+if [ ! -d "$themes_dir" ]; then
+    echo "Директория $themes_dir не найдена. Создаём..."
+    mkdir -p "$themes_dir"
+    echo "Директория $themes_dir успешно создана."
+else
+    echo "Директория $themes_dir уже существует."
+fi
+cd /home/$USER/.themes && git clone --depth=1 https://github.com/vinceliuice/Orchis-theme.git && cd Orchis-theme/ && bash ./install.sh -c dark -s compact --tweaks dracula --round 3 && cd
+
+# Icons
+# Путь к директории
+icons_dir="/home/$username/.icons"
+
+# Проверяем, существует ли директория, и если нет — создаём её
+if [ ! -d "$icons_dir" ]; then
+    echo "Директория $icons_dir не найдена. Создаём..."
+    mkdir -p "$themes_dir"
+    echo "Директория $icons_dir успешно создана."
+else
+    echo "Директория $icons_dir уже существует."
+fi
+cd /home/$USER/.icons && git clone --depth=1 https://github.com/vinceliuice/Tela-circle-icon-theme.git && cd Tela-circle-icon-theme/ && bash ./install.sh dracula -c && cd
+
+echo "Themes & Icons Installed!"
+
 echo ""
 echo "✅ Установка успешно завершена!"
 echo "💡 Следующие шаги:"

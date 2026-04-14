60ee5d7693033c10538944a2937cf410425a8029
laimusss <48787750+laimusss@users.noreply.github.com>
2026-04-14 11:07:47 +0300

    Update mint-uninstall.sh

--- mint-uninstall.sh
+++ mint-uninstall.sh
@@ -2,17 +2,30 @@
 set -e
 
 # Удаление пакетов
-sudo apt purge -y celluloid hypnotix rhythmbox* drawing pix simple-scan firefox* thunderbird* transmission* thingy sticky libreoffice* onboard warpinator
+sudo apt purge -y celluloid hypnotix rhythmbox* drawing pix simple-scan firefox* thunderbird* 
+transmission* thingy sticky libreoffice* onboard warpinator
 
 sudo apt autoremove -y && sudo apt autoclean
 
 # Установка темы и иконок
-mkdir -p $HOME/.themes $HOME/.icons
+# Создаём директории
+mkdir -p "$HOME/.themes"
+mkdir -p "$HOME/.icons"
 
-git clone --depth=1 https://github.com/vinceliuice/Orchis-theme.git $HOME/.themes/ -q
-bash $HOME/.themes/Orchis-theme/install.sh -c dark -s compact --tweaks dracula --round 3 -q
-rm -rf $HOME/.themes/Orchis-theme
+# Клонируем тему (если директория уже есть - удаляем её)
+if [ -d "$HOME/.themes/Orchis-theme" ]; then
+    rm -rf "$HOME/.themes/Orchis-theme"
+fi
+git clone --depth=1 https://github.com/vinceliuice/Orchis-theme.git 
+"$HOME/.themes/Orchis-theme" -q
+bash "$HOME/.themes/Orchis-theme/install.sh" -c dark -s compact --tweaks dracula --round 3 -q
+rm -rf "$HOME/.themes/Orchis-theme"
 
-git clone --depth=1 https://github.com/vinceliuice/Tela-circle-icon-theme.git $HOME/.icons/ -q
-bash $HOME/.icons/Tela-circle-icon-theme/install.sh dracula -c -q
-rm -rf $HOME/.icons/Tela-circle-icon-theme
+# Клонируем иконки (если директория уже есть - удаляем её)
+if [ -d "$HOME/.icons/Tela-circle-icon-theme" ]; then
+    rm -rf "$HOME/.icons/Tela-circle-icon-theme"
+fi
+git clone --depth=1 https://github.com/vinceliuice/Tela-circle-icon-theme.git 
+"$HOME/.icons/Tela-circle-icon-theme" -q
+bash "$HOME/.icons/Tela-circle-icon-theme/install.sh" dracula -c -q
+rm -rf "$HOME/.icons/Tela-circle-icon-theme"

17c7676292f735f78cd658e0f4876b9a2130879c
laimusss <48787750+laimusss@users.noreply.github.com>
2026-04-14 11:00:33 +0300

    Update mint-uninstall.sh

--- mint-uninstall.sh
+++ mint-uninstall.sh
@@ -1,63 +1,20 @@
 #!/bin/bash
+set -e
 
-# Скрипт для удаления ненужных пакетов
-# Создаем резервную копию списка установленных пакетов
-echo "Создание резервной копии списка пакетов..."
-dpkg --get-selections > ~/packages-list-$(date +%Y%m%d-%H%M%S).txt
+# Удаление пакетов
+sudo apt purge -y celluloid hypnotix rhythmbox* drawing pix simple-scan firefox* thunderbird* transmission* thingy sticky libreoffice* onboard warpinator
 
-Альтернативный вариант с более точным указанием пакетов:
-sudo apt purge -y \
-     celluloid \
-     hypnotix \
-     rhythmbox rhythmbox-plugins \
-     drawing \
-     pix \
-     simple-scan \
-     firefox firefox-locale-* \
-     thunderbird thunderbird-locale-* \
-     transmission-gtk transmission-cli transmission-common \
-     thingy \
-     sticky \
-     libreoffice-core libreoffice-common \
-     onboard \
-     warpinator
+sudo apt autoremove -y && sudo apt autoclean
 
-echo "Удаление зависимостей, которые больше не нужны..."
-sudo apt autoremove -y
+# Установка темы и иконок
+mkdir -p ~/.themes ~/.icons
 
-echo "Очистка кэша пакетов..."
-sudo apt autoclean
+git clone --depth=1 https://github.com/vinceliuice/Orchis-theme.git ~/.themes/ -q
+bash ~/.themes/Orchis-theme/install.sh -c dark -s compact --tweaks dracula --round 3 -q
+rm -rf ~/.themes/Orchis-theme
 
-echo "Очистка завершена!"
-
-# Themes
-# Путь к директории
-themes_dir="/home/$username/.themes"
-
-# Проверяем, существует ли директория, и если нет — создаём её
-if [ ! -d "$themes_dir" ]; then
-    echo "Директория $themes_dir не найдена. Создаём..."
-    mkdir -p "$themes_dir"
-    echo "Директория $themes_dir успешно создана."
-else
-    echo "Директория $themes_dir уже существует."
-fi
-cd /home/$USER/.themes && git clone --depth=1 https://github.com/vinceliuice/Orchis-theme.git && cd Orchis-theme/ && bash ./install.sh -c dark -s compact --tweaks dracula --round 3 && cd
-
-# Icons
-# Путь к директории
-icons_dir="/home/$username/.icons"
-
-# Проверяем, существует ли директория, и если нет — создаём её
-if [ ! -d "$icons_dir" ]; then
-    echo "Директория $icons_dir не найдена. Создаём..."
-    mkdir -p "$themes_dir"
-    echo "Директория $icons_dir успешно создана."
-else
-    echo "Директория $icons_dir уже существует."
-fi
-cd /home/$USER/.icons && git clone --depth=1 https://github.com/vinceliuice/Tela-circle-icon-theme.git && cd Tela-circle-icon-theme/ && bash ./install.sh dracula -c && cd
-
-echo "Themes & Icons Installed!"
+git clone --depth=1 https://github.com/vinceliuice/Tela-circle-icon-theme.git ~/.icons/ -q
+bash ~/.icons/Tela-circle-icon-theme/install.sh dracula -c -q
+rm -rf ~/.icons/Tela-circle-icon-theme
 
 

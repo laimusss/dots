60c863bb79d4836e984a774c7ef34ffb190c9f3a
laimusss <48787750+laimusss@users.noreply.github.com>
2026-04-14 11:10:47 +0300

    Update mint-uninstall.sh

--- mint-uninstall.sh
+++ mint-uninstall.sh
@@ -1,30 +1,33 @@
 #!/bin/bash
 set -e
 
+# Определяем HOME (на случай если запущено через sudo)
+export HOME="$(eval echo ~$(whoami))"
+
 # Удаление пакетов
 sudo apt purge -y celluloid hypnotix rhythmbox* drawing pix simple-scan firefox* thunderbird* transmission* thingy sticky libreoffice* onboard warpinator
 
 sudo apt autoremove -y && sudo apt autoclean
 
 # Установка темы и иконок
-# Создаём директории
-mkdir -p "$HOME/.themes"
-mkdir -p "$HOME/.icons"
+mkdir -p "${HOME}/.themes"
+mkdir -p "${HOME}/.icons"
 
-# Клонируем тему (если директория уже есть - удаляем её)
-if [ -d "$HOME/.themes/Orchis-theme" ]; then
-    rm -rf "$HOME/.themes/Orchis-theme"
+# Тема
+if [ -d "${HOME}/.themes/Orchis-theme" ]; then
+    rm -rf "${HOME}/.themes/Orchis-theme"
 fi
 git clone --depth=1 https://github.com/vinceliuice/Orchis-theme.git 
-"$HOME/.themes/Orchis-theme" -q
-bash "$HOME/.themes/Orchis-theme/install.sh" -c dark -s compact --tweaks dracula --round 3 -q
-rm -rf "$HOME/.themes/Orchis-theme"
+"${HOME}/.themes/Orchis-theme" -q
+bash "${HOME}/.themes/Orchis-theme/install.sh" -c dark -s compact --tweaks dracula --round 3 
+-q
+rm -rf "${HOME}/.themes/Orchis-theme"
 
-# Клонируем иконки (если директория уже есть - удаляем её)
-if [ -d "$HOME/.icons/Tela-circle-icon-theme" ]; then
-    rm -rf "$HOME/.icons/Tela-circle-icon-theme"
+# Иконки
+if [ -d "${HOME}/.icons/Tela-circle-icon-theme" ]; then
+    rm -rf "${HOME}/.icons/Tela-circle-icon-theme"
 fi
 git clone --depth=1 https://github.com/vinceliuice/Tela-circle-icon-theme.git 
-"$HOME/.icons/Tela-circle-icon-theme" -q
-bash "$HOME/.icons/Tela-circle-icon-theme/install.sh" dracula -c -q
-rm -rf "$HOME/.icons/Tela-circle-icon-theme"
+"${HOME}/.icons/Tela-circle-icon-theme" -q
+bash "${HOME}/.icons/Tela-circle-icon-theme/install.sh" dracula -c -q
+rm -rf "${HOME}/.icons/Tela-circle-icon-theme"
